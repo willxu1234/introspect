1ee4279f8c31f28a83b2051ff4c3b915670ac877
Will Xu <willxu1234@gmail.com>
2020-09-14 00:53:20 -0700

    add love, fix joy color

--- lib/models/emotions.dart
+++ lib/models/emotions.dart
@@ -14,65 +14,65 @@ class Emotion {
 class Emotions {
   // TODO: Add actual color interpolation.
 
-  // Joyful, level 3.
-  static final Emotion jubilant = Emotion("Jubilant", Colors.yellow, HashMap());
-  static final Emotion elated = Emotion("Elated", Colors.yellow, HashMap());
-  static final Emotion zealous = Emotion("Zealous", Colors.yellow, HashMap());
+  // Joy, level 3.
+  static final Emotion jubilant = Emotion("Jubilant", Colors.green, HashMap());
+  static final Emotion elated = Emotion("Elated", Colors.green, HashMap());
+  static final Emotion zealous = Emotion("Zealous", Colors.green, HashMap());
   static final Emotion enthusiastic =
-      Emotion("Enthusiastic", Colors.yellow, HashMap());
-  static final Emotion hopeful = Emotion("Hopeful", Colors.yellow, HashMap());
-  static final Emotion eager = Emotion("Eager", Colors.yellow, HashMap());
+      Emotion("Enthusiastic", Colors.green, HashMap());
+  static final Emotion hopeful = Emotion("Hopeful", Colors.green, HashMap());
+  static final Emotion eager = Emotion("Eager", Colors.green, HashMap());
   static final Emotion illustrious =
-      Emotion("Illustrious", Colors.yellow, HashMap());
+      Emotion("Illustrious", Colors.green, HashMap());
   static final Emotion triumphant =
-      Emotion("Triumphant", Colors.yellow, HashMap());
-  static final Emotion playful = Emotion("Playful", Colors.yellow, HashMap());
-  static final Emotion amused = Emotion("Amused", Colors.yellow, HashMap());
+      Emotion("Triumphant", Colors.green, HashMap());
+  static final Emotion playful = Emotion("Playful", Colors.green, HashMap());
+  static final Emotion amused = Emotion("Amused", Colors.green, HashMap());
   static final Emotion delighted =
-      Emotion("Delighted", Colors.yellow, HashMap());
-  static final Emotion jovial = Emotion("Jovial", Colors.yellow, HashMap());
-  static final Emotion pleased = Emotion("Pleased", Colors.yellow, HashMap());
+      Emotion("Delighted", Colors.green, HashMap());
+  static final Emotion jovial = Emotion("Jovial", Colors.green, HashMap());
+  static final Emotion pleased = Emotion("Pleased", Colors.green, HashMap());
   static final Emotion satisfied =
-      Emotion("Satisfied", Colors.yellow, HashMap());
-  static final Emotion serene = Emotion("Serene", Colors.yellow, HashMap());
-  static final Emotion tranquil = Emotion("Tranquil", Colors.yellow, HashMap());
+      Emotion("Satisfied", Colors.green, HashMap());
+  static final Emotion serene = Emotion("Serene", Colors.green, HashMap());
+  static final Emotion tranquil = Emotion("Tranquil", Colors.green, HashMap());
 
-// Joyful, level 2.
-  static final Emotion euphoric = Emotion("Euphoric", Colors.yellow, {
+// Joy, level 2.
+  static final Emotion euphoric = Emotion("Euphoric", Colors.green, {
     "jubliant": jubilant,
     "elated": elated,
   });
-  static final Emotion excited = Emotion("Excited", Colors.yellow, {
+  static final Emotion excited = Emotion("Excited", Colors.green, {
     "zealous": zealous,
     "enthusiastic": enthusiastic,
   });
-  static final Emotion optimistic = Emotion("Optimistic", Colors.yellow, {
+  static final Emotion optimistic = Emotion("Optimistic", Colors.green, {
     "hopeful": hopeful,
     "eager": eager,
   });
-  static final Emotion proud = Emotion("Proud", Colors.yellow, {
+  static final Emotion proud = Emotion("Proud", Colors.green, {
     "illustrious": illustrious,
     "triumphant": triumphant,
   });
-  static final Emotion cheerful = Emotion("Cheerful", Colors.yellow, {
+  static final Emotion cheerful = Emotion("Cheerful", Colors.green, {
     "playful": playful,
     "amused": amused,
   });
-  static final Emotion happy = Emotion("Happy", Colors.yellow, {
+  static final Emotion happy = Emotion("Happy", Colors.green, {
     "delighted": delighted,
     "jovial": jovial,
   });
-  static final Emotion content = Emotion("Content", Colors.yellow, {
+  static final Emotion content = Emotion("Content", Colors.green, {
     "pleased": pleased,
     "satisfied": satisfied,
   });
-  static final Emotion peaceful = Emotion("Peaceful", Colors.yellow, {
+  static final Emotion peaceful = Emotion("Peaceful", Colors.green, {
     "serene": serene,
     "tranquil": tranquil,
   });
 
-  // Joyful, level 1.
-  static final Emotion joyful = Emotion("Joyful", Colors.yellow, {
+  // Joy, level 1.
+  static final Emotion joy = Emotion("Joyful", Colors.green, {
     "euphoric": euphoric,
     "excited": excited,
     "optimistic": optimistic,
@@ -83,8 +83,58 @@ class Emotions {
     "peaceful": peaceful,
   });
 
+  // Love, level 3.
+  static final Emotion enthralled = Emotion("Enthralled", Colors.yellow, {});
+  static final Emotion rapturous = Emotion("Rapturous", Colors.yellow, {});
+  static final Emotion passionate = Emotion("Passionate", Colors.yellow, {});
+  static final Emotion enamored = Emotion("Enamored", Colors.yellow, {});
+  static final Emotion warmhearted = Emotion("Warmhearted", Colors.yellow, {});
+  static final Emotion compassionate =
+      Emotion("Compassionate", Colors.yellow, {});
+  static final Emotion tender = Emotion("Tender", Colors.yellow, {});
+  static final Emotion nostalgic = Emotion("Nostalgic", Colors.yellow, {});
+  static final Emotion appreciative =
+      Emotion("Appreciative", Colors.yellow, {});
+  static final Emotion thankful = Emotion("Thankful", Colors.yellow, {});
+
+  // Love, level 2.
+  static final Emotion enchanted = Emotion("Enchanted", Colors.yellow, {
+    "enthralled": enthralled,
+    "rapturous": rapturous,
+  });
+  static final Emotion romantic = Emotion("Romantic", Colors.yellow, {
+    "passionate": passionate,
+    "enamored": enamored,
+  });
+  static final Emotion affectionate = Emotion("Affectionate", Colors.yellow, {
+    "warmhearted": warmhearted,
+    "compassionate": compassionate,
+  });
+  static final Emotion sentimental = Emotion("Sentimental", Colors.yellow, {
+    "tender": tender,
+    "nostalgic": nostalgic,
+  });
+  static final Emotion grateful = Emotion("Grateful", Colors.yellow, {
+    "appreciateive": appreciative,
+    "thankful": thankful,
+  });
+
+  // Love, level 1.
+  static final Emotion love = Emotion("Love", Colors.yellow, {
+    "grateful": grateful,
+    "sentimental": sentimental,
+    "affectionate": affectionate,
+    "romantic": romantic,
+    "enchanted": enchanted,
+  });
+
   // The beeg emotion wheel.
   static final Emotion wheel = Emotion("default", Colors.white, {
-    "joyful": joyful,
+    "joy": joy,
+    "love": love,
+    // "fear": fear,
+    // "anger": anger,
+    // "sadness": sadness,
+    // "surprise": surprise,
   });
 }
