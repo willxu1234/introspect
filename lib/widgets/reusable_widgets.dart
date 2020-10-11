aae485cb20ffe19c65e994fcdf83968717a3e854
Will Xu <willxu1234@gmail.com>
2020-10-11 15:14:11 -0700

    remove labels

--- lib/widgets/reusable_widgets.dart
+++ lib/widgets/reusable_widgets.dart
@@ -33,6 +33,8 @@ class BottomNavigation extends StatelessWidget {
       ],
       currentIndex: routeIndex,
       onTap: onTap,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
     );
   }
 }
